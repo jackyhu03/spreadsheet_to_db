aaba47b7727624d773b86fae65bc73cd1e3b2da9
Aspideveleno <77437585+Aspideveleno@users.noreply.github.com>
2022-03-03 13:54:24 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,10 +1,19 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
     $values = parse_url($_REQUEST["url"]);
-
     $path = explode('/', $values["path"]);
-
     $key = $path[3];
-
     echo $key;
-
+}
+else{
+?>
+<html>
+    <form class="form" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    link del foglio:<input id="ins-link" type="text" name="url" required>
+    <button type="submit" class="mandalink">manda link</button>
+    </form>
+</html>
+<?php
+}
 ?>
