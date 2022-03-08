e29ad60e10f59a7fe83a9808d3a627a1789316f8
Migliorini Gabriele <Migliorini.17191>
2022-03-08 09:23:10 +0100

    method fixed

--- main.php
+++ main.php
@@ -6,29 +6,27 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
 
     <body>
         <div>
-            <input id="ins-link" type="text" id="spreadsheet_url" placeholder="googlesheet url" required>
+            <input id="spreadsheet_url" type="text"  placeholder="googlesheet url" required>
             <button id="BTN" >manda link</button>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </body>
 
     <script>
 
         $('#BTN').on('click', () => {
-            alert("");
-            alert( $('#spreadsheet_url').html() );
-            req ( $('#spreadsheet_url').html() );
+            req ('GET', './spreadtodb.php', { spreadsheet_url: $('#spreadsheet_url').val()} );
         });
 
-        const req = (data) => {
+        const req = (method, url, data) => {
 
             $.ajax({
-                type: 'GET',
-                url: './spreadtodb.php',
+                type: method,
+                url: url,
                 data: data,
                 success: (response) => {
                     console.log(response);
